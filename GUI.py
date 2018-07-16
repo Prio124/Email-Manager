5c3f54827739524ad880c4e0ca8a3cd1a705aeb3
Prio124 <priolosalvatore1@libero.it>
2018-07-16 09:55:47 +0200

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,65 @@
+from Tkinter import *
+import smtplib
+
+class MainApp:
+    def __init__(self, root):
+        #Top frame
+        self.frame = Frame(root)
+        self.frame.grid(column=0, row=0, columnspan=4, rowspan=4)
+        #label 1 { Email : }
+        self.lbl1 = Label(self.frame, text="Email :")
+        self.lbl1.grid(column=0, row=0)
+        #email
+        self.email = StringVar()
+        self.email.set("email...")
+        self.ent1 = Entry(self.frame, textvariable=self.email)
+        self.ent1.grid(column=1, row=0)
+        #label 2 { Password : } 
+        self.lbl2 = Label(self.frame, text="Password :")
+        self.lbl2.grid(column=0, row=1)
+        #password
+        self.password = StringVar()
+        self.ent2 = Entry(self.frame, show="*", textvariable=self.password)
+        self.ent2.grid(column=1, row=1)
+        #control label
+        self.controlbl = Label(self.frame, text="")
+        self.controlbl.grid(column=0, row=2)
+        #button
+        self.checkButton = Button(self.frame, text="LOGIN")
+        self.checkButton.grid(column=1, row=2)
+        self.checkButton.bind("<Button-1>", self.login)
+
+    def login(self, event):
+        try:
+            server = smtplib.SMTP_SSL("smtp.libero.it", 465)
+            server.login(self.email.get(), self.password.get())
+            #open new window
+            window = Tkinter.TopLevel(root)
+            
+        except:
+            self.controlbl["text"] = "WRONG DATA!"
+
+class SendEmail:
+    def __init__(self, root):
+        #Top frame
+        self.frame = Frame(root)
+        self.frame.grid(column=0, row=0, columnspan=4, rowspan=4)
+        #label 1 { Email : }
+        self.lbl1 = Label(self.frame, text="Email :")
+        self.lbl1.grid(column=0, row=0)
+
+
+            
+        
+
+def main():
+    root = Tk()
+    root.title("Email Manager")
+    mainApp = MainApp(root)
+    root.mainloop()
+
+main()
+
+
+    
+    
